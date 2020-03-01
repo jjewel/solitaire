cf605b410c5bd0c786a767101014bae7bf3d9b1c
jjewel <jjewe2009@gmail.com>
2020-03-01 19:03:29 +1000

    add icons and draw mechanics

--- Assets/Scripts/Solitaire.cs
+++ Assets/Scripts/Solitaire.cs
@@ -9,14 +9,18 @@ public class Solitaire : MonoBehaviour
     public GameObject cardPrefab;
     public GameObject[] bottomPos;
     public GameObject[] topPos;
+    public GameObject deckButton;
 
-    public static string[] suits = new string[] { "C", "D", "H", "S"};
-    public static string[] values = new string[] { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"};
-    public static string[] extras = new string[] { "JR", "JB"};
+    public static string[] suits = new string[] { "C", "D", "H", "S" };
+    public static string[] values = new string[] { "A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K" };
+    public static string[] extras = new string[] { "JR", "JB" };
 
     public List<string> deck;
+    public List<string> discardPile = new List<string>();
     public List<string>[] bottoms;
     public List<string>[] tops;
+    public List<string> tripsOnDisplay = new List<string>();
+    public List<List<string>> deckTrips = new List<List<string>>();
 
     private List<string> bottom0 = new List<string>();
     private List<string> bottom1 = new List<string>();
@@ -25,6 +29,10 @@ public class Solitaire : MonoBehaviour
     private List<string> bottom4 = new List<string>();
     private List<string> bottom5 = new List<string>();
     private List<string> bottom6 = new List<string>();
+
+    private int trips;
+    private int tripsRemainder;
+    private int deckLocation;
 
     // Start is called before the first frame update
     void Start()
@@ -36,7 +44,7 @@ public class Solitaire : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     public void PlayCards()
@@ -46,7 +54,8 @@ public class Solitaire : MonoBehaviour
         //ShuffleSeeded(deck, 0);
 
         SolitaireSort();
-        SolitaireDeal();
+        StartCoroutine(SolitaireDeal());
+        SortDeckIntoTrips();
     }
 
     public static List<string> GenerateDeck(bool addExtras = false)
@@ -74,7 +83,7 @@ public class Solitaire : MonoBehaviour
     void Shuffle<T>(List<T> list)
     {
         System.Random random = new System.Random();
-             
+
         int n = list.Count;
         while (n > 1)
         {
@@ -101,7 +110,7 @@ public class Solitaire : MonoBehaviour
         }
     }
 
-    void SolitaireDeal()
+    IEnumerator SolitaireDeal()
     {
         for (int i = 0; i < 7; i++)
         {
@@ -109,6 +118,7 @@ public class Solitaire : MonoBehaviour
             float zOffset = 0.03f;
             foreach (string card in bottoms[i])
             {
+                yield return new WaitForSeconds(0.01f);
                 GameObject newCard = Instantiate(
                     cardPrefab,
                     new Vector3(
@@ -124,12 +134,22 @@ public class Solitaire : MonoBehaviour
                 {
                     newCard.GetComponent<Selectable>().faceUp = true;
                 }
-                
+
 
                 yOffset += 0.3f;
                 zOffset += 0.03f;
+                discardPile.Add(card);
             }
         }
+
+        foreach (string card in discardPile)
+        {
+            if (deck.Contains(card))
+            {
+                deck.Remove(card);
+            }
+        }
+        discardPile.Clear();
     }
 
     void SolitaireSort()
@@ -142,5 +162,87 @@ public class Solitaire : MonoBehaviour
                 deck.RemoveAt(deck.Count - 1);
             }
         }
+    }
+
+    public void SortDeckIntoTrips()
+    {
+        trips = deck.Count / 3;
+        tripsRemainder = deck.Count % 3;
+        deckTrips.Clear();
+
+        int modifier = 0;
+        for (int i = 0; i < trips; i++)
+        {
+            List<string> myTrips = new List<string>();
+            for (int j = 0; j < 3; j++)
+            {
+                myTrips.Add(deck[j + modifier]);
+            }
+            deckTrips.Add(myTrips);
+            modifier += 3;
+        }
+
+        if (tripsRemainder != 0)
+        {
+            List<string> myRemainders = new List<string>();
+            modifier = 0;
+            for (int k = 0; k < tripsRemainder; k++)
+            {
+                myRemainders.Add(deck[deck.Count - tripsRemainder + modifier]);
+                modifier++;
+            }
+            deckTrips.Add(myRemainders);
+            trips++;
+        }
+        deckLocation = 0;
+    }
+
+    public void DealFromDeck()
+    {
+        // add remaining cards to discard pile
+        foreach (Transform child in deckButton.transform)
+        {
+            if (child.CompareTag("Card"))
+            {
+                discardPile.Add(child.name);
+                deck.Remove(child.name);
+                Destroy(child.gameObject);
+            }
+
+        }
+
+        if (deckLocation < trips)
+        {
+            tripsOnDisplay.Clear();
+            float xOffset = 2.5f;
+            float zOffset = -0.2f;
+
+            foreach (string card in deckTrips[deckLocation])
+            {
+                GameObject newTopCard = Instantiate(cardPrefab, new Vector3(deckButton.transform.position.x + xOffset, deckButton.transform.position.y, deckButton.transform.position.z + zOffset), Quaternion.identity, deckButton.transform);
+                newTopCard.transform.localScale = new Vector3(1f, 1f, 1f);
+                xOffset += 0.5f;
+                zOffset += -0.2f;
+                newTopCard.name = card;
+                tripsOnDisplay.Add(card);
+                newTopCard.GetComponent<Selectable>().faceUp = true;
+            }
+            deckLocation++;
+
+        }
+        else
+        {
+            RestackTopDeck();
+        }
+    }
+
+    void RestackTopDeck()
+    {
+        foreach (string card in discardPile)
+        {
+            deck.Add(card);
+        }
+        discardPile.Clear();
+        SortDeckIntoTrips();
     }
 }
--- Assets/Scripts/UserInput.cs
+++ Assets/Scripts/UserInput.cs
@@ -4,15 +4,66 @@ using UnityEngine;
 
 public class UserInput : MonoBehaviour
 {
+    private Solitaire solitaire;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        solitaire = FindObjectOfType<Solitaire>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        GetMouseClick();
+    }
+
+    void GetMouseClick()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 mousePosition = Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, -10));
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+            if (hit)
+            {
+                // What has been hit?
+                if (hit.collider.CompareTag("Deck"))
+                {
+                    Deck();
+                } else if (hit.collider.CompareTag("Card"))
+                {
+                    Card();
+                }
+                else if (hit.collider.CompareTag("Top"))
+                {
+                    Top();
+                }
+                else if (hit.collider.CompareTag("Bottom"))
+                {
+                    Bottom();
+                }
+            }
+        }
+    }
+
+    void Deck()
+    {
+        Debug.Log("Clicked on Deck");
+        solitaire.DealFromDeck();
+    }
+
+    void Card()
+    {
+        Debug.Log("Clicked on Card");
+    }
+
+    void Top()
+    {
+        Debug.Log("Clicked on Top");
+    }
+
+    void Bottom()
+    {
+        Debug.Log("Clicked on Bottom");
     }
 }
